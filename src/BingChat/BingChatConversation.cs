de4daddb778117a3cfc8e5a0d515fa1429851cb5
Awbugl <me@awbugl.top>
2023-02-16 16:28:43 +0800

    Update BingChatConversation.cs

--- src/BingChat/BingChatConversation.cs
+++ src/BingChat/BingChatConversation.cs
@@ -25,71 +25,73 @@ internal sealed class BingChatConversation : IBingChattable
     {
         var wsClient = new WebsocketClient(new Uri("wss://sydney.bing.com/sydney/ChatHub"));
         var tcs = new TaskCompletionSource<string>();
-        var answer = string.Empty;
 
         void Cleanup()
         {
-            wsClient.Stop(WebSocketCloseStatus.Empty, string.Empty).GetAwaiter().GetResult();
-            wsClient.Dispose();
+            wsClient.Stop(WebSocketCloseStatus.Empty, string.Empty).ContinueWith(t =>
+            {
+                if (t.IsFaulted) tcs.SetException(t.Exception!);
+                wsClient.Dispose();
+            });
+        }
+
+        string? GetAnswer(BingChatConversationResponse response)
+        {
+            for (var index = response.Item.Messages.Length - 1; index >= 0; index--)
+            {
+                var itemMessage = response.Item.Messages[index];
+                if (itemMessage.MessageType != null) continue;
+                if (itemMessage.Author != "bot") continue;
+
+                // maybe is possible to use itemMessage.Text directly, but some extra information will be lost
+                return itemMessage.AdaptiveCards[0].Body[0].Text;
+            }
+
+            return null;
+        }
+
+        void OnMessageReceived(string text)
+        {
+            try
+            {
+                foreach (var part in text.Split(TerminalChar, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    var json = JsonSerializer.Deserialize<BingChatConversationResponse>(part);
+
+                    if (json is not { Type: 2 }) continue;
+
+                    Cleanup();
+
+                    tcs.SetResult(GetAnswer(json) ?? "<empty answer>");
+                    return;
+                }
+            }
+            catch (Exception e)
+            {
+                Cleanup();
+                tcs.SetException(e);
+            }
         }
 
         wsClient.MessageReceived
-            .Where(msg => msg.MessageType == WebSocketMessageType.Text)
-            .Select(msg => msg.Text)
-            .Subscribe(text =>
-            {
-                try
-                {
-                    foreach (var part in text.Split(TerminalChar, StringSplitOptions.RemoveEmptyEntries))
-                    {
-                        var document = JsonDocument.Parse(part);
-                        if (!document.RootElement.TryGetProperty("type", out var typeElement))
-                            continue;
-
-                        var type = typeElement.GetInt32();
-
-                        switch (type)
-                        {
-                            case 1:
-                                // Update the answer
-                                try
-                                {
-                                    if (
-                                        document.RootElement.TryGetProperty("arguments", out var args)
-                                        && args[0].TryGetProperty("messages", out var msgs)
-                                        && msgs[0].TryGetProperty("adaptiveCards", out var cards)
-                                        && cards[0].TryGetProperty("body", out var body)
-                                        && body[0].TryGetProperty("text", out var txt))
-                                        answer = txt.GetString();
-                                }
-                                catch (Exception e)
-                                {
-                                    Console.WriteLine(e);
-                                }
-
-                                break;
-
-                            case 2:
-                                // Received terminal message, cleanup
-                                Cleanup();
-                                tcs.SetResult(answer ?? "<empty answer>");
-                                return;
-                        }
-                    }
-                }
-                catch (Exception e)
-                {
-                    Cleanup();
-                    tcs.SetException(e);
-                }
-            });
+                .Where(msg => msg.MessageType == WebSocketMessageType.Text)
+                .Select(msg => msg.Text)
+                .Subscribe(OnMessageReceived);
 
         // Start the WebSocket client
-        wsClient.Start().GetAwaiter().GetResult();
+        wsClient.Start().ContinueWith(t =>
+        {
+            if (t.IsFaulted)
+            {
+                Cleanup();
+                tcs.SetException(t.Exception!);
+                return;
+            }
 
-        // Send initial messages
-        wsClient.Send("{\"protocol\":\"json\",\"version\":1}" + TerminalChar);
-        wsClient.Send(_request.ConstructInitialPayload(message) + TerminalChar);
+            // Send initial messages
+            wsClient.Send("{\"protocol\":\"json\",\"version\":1}" + TerminalChar);
+            wsClient.Send(_request.ConstructInitialPayload(message) + TerminalChar);
+        });
 
         return tcs.Task;
     }
---
+++ src/BingChat/BingChatConversationResponse.cs
@@ -0,0 +1,57 @@
+﻿using System.Text.Json.Serialization;
+
+// ReSharper disable MemberCanBeInternal
+// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+
+#pragma warning disable CS8618
+
+namespace BingChat;
+
+public sealed class BingChatConversationResponse
+{
+    [JsonPropertyName("type")]
+    public int Type { get; set; }
+
+    [JsonPropertyName("item")]
+    public Item Item { get; set; }
+}
+
+public sealed class Item
+{
+    [JsonPropertyName("messages")]
+    public Message[] Messages { get; set; }
+}
+
+public sealed class Message
+{
+    [JsonPropertyName("text")]
+    public string Text { get; set; }
+
+    [JsonPropertyName("author")]
+    public string Author { get; set; }
+
+    [JsonPropertyName("messageType")]
+    public string? MessageType { get; set; }
+
+    [JsonPropertyName("adaptiveCards")]
+    public AdaptiveCard[] AdaptiveCards { get; set; }
+}
+
+public sealed class AdaptiveCard
+{
+    [JsonPropertyName("body")]
+    public Body[] Body { get; set; }
+}
+
+public sealed class Body
+{
+    [JsonPropertyName("type")]
+    public string Type { get; set; }
+
+    [JsonPropertyName("text")]
+    public string Text { get; set; }
+
+    [JsonPropertyName("wrap")]
+    public bool Wrap { get; set; }
+}
