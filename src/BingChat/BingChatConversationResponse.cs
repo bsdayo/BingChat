e0b74dd30db1b57cb80f18c59c1de320e4976d04
neon-sunset <neon_sunset@outlook.com>
2023-05-29 15:45:23 +0300

    feat: signalr transport

--- src/BingChat/BingChatConversationResponse.cs
+++ src/BingChat/BingChatConversationResponse.cs
@@ -1,6 +1,4 @@
-﻿using System.Text.Json.Serialization;
-
-// ReSharper disable MemberCanBeInternal
+﻿// ReSharper disable MemberCanBeInternal
 // ReSharper disable ClassNeverInstantiated.Global
 // ReSharper disable UnusedAutoPropertyAccessor.Global
 
@@ -10,69 +8,38 @@ namespace BingChat;
 
 internal sealed class BingChatConversationResponse
 {
-    [JsonPropertyName("type")]
-    public int Type { get; set; }
-
-    [JsonPropertyName("item")]
-    public ResponseItem Item { get; set; }
-}
-
-internal sealed class ResponseItem
-{
-    [JsonPropertyName("messages")]
     public ResponseMessage[] Messages { get; set; }
-
-    [JsonPropertyName("result")]
     public ResponseResult Result { get; set; }
 }
 
 internal sealed class ResponseMessage
 {
-    [JsonPropertyName("text")]
     public string? Text { get; set; }
-
-    [JsonPropertyName("author")]
     public string Author { get; set; }
-
-    [JsonPropertyName("messageType")]
     public string? MessageType { get; set; }
-
-    [JsonPropertyName("adaptiveCards")]
     public AdaptiveCard[]? AdaptiveCards { get; set; }
-
-    [JsonPropertyName("sourceAttributions")]
     public SourceAttribution[]? SourceAttributions { get; set; }
 }
 
 internal sealed class AdaptiveCard
 {
-    [JsonPropertyName("body")]
     public ResponseBody[] Body { get; set; }
 }
 
 internal sealed class ResponseBody
 {
-    [JsonPropertyName("type")]
     public string Type { get; set; }
-
-    [JsonPropertyName("text")]
     public string? Text { get; set; }
 }
 
 internal sealed class SourceAttribution
 {
-    [JsonPropertyName("providerDisplayName")]
     public string ProviderDisplayName { get; set; }
-
-    [JsonPropertyName("seeMoreUrl")]
     public string SeeMoreUrl { get; set; }
 }
 
 internal sealed class ResponseResult
 {
-    [JsonPropertyName("value")]
     public string Value { get; set; }
-
-    [JsonPropertyName("message")]
     public string? Message { get; set; }
 }
